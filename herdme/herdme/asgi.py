39c98bd875df1f98ed0276458481892f2533c162
bjern-potgieter <bjern.potgieter@byteorbit.com>
2021-05-05 13:53:26 +0200

    Initial project files

---
+++ herdme/herdme/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for herdme project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'herdme.settings')
+
+application = get_asgi_application()
